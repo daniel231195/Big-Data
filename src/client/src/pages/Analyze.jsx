b3726957b8a53d33db6b7baddb914fc6bf0ce5d0
rafasu4 <raf59101@gmail.com>
2023-02-09 12:03:22 +0200

    💄Sidenavbar

---
+++ src/client/src/pages/Analyze.jsx
@@ -0,0 +1,13 @@
+import styled from "styled-components";
+
+const Analyze = () => {
+    return(
+        <h1>Analyze</h1>
+    )
+};
+
+const EmptyPageStyle = styled.div`
+    // height: 100vh;
+    
+`;
+export default Analyze;
---
+++ src/client/src/pages/Search.jsx
@@ -0,0 +1,13 @@
+import styled from "styled-components";
+
+const Search = () => {
+    return(
+        <h1>Search</h1>
+    )
+};
+
+const EmptyPageStyle = styled.div`
+    // height: 100vh;
+    
+`;
+export default Search;
