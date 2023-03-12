5b80b2abb6b4cf07e26961909c1c740bd489d635
Stycks4 <raphael.suliman@globekeeper.com>
2023-03-12 19:17:16 +0200

    💄dashboard

---
+++ src/client/src/pages/Dashboard.styled.jsx
@@ -0,0 +1,15 @@
+import styled from "styled-components";
+
+export const DashboardWrapper = styled.div`
+    margin-top: 5%;
+    display: flex;
+    flex-direction: column;
+    width: 100%;
+    gap: 15%;
+`;
+
+export const DashboardCardsWrapper = styled.div`
+    display: flex;
+    flex-wrap: wrap;
+    gap: 10px;
+`;
