351227860323226503cd2851d710eed4b3c8873f
JosefTheKing17 <yosimami50@gmail.com>
2023-03-13 16:02:42 +0200

    For Josef Sokolov

--- src/server/ServingLayer/config/kafka.config.js
+++ src/server/ServingLayer/config/kafka.config.js
@@ -1,5 +1,15 @@
-const kafkaConf = {
-  "group.id": "fflvkrwz-group2",
+const kafkaConfigElastic = {
+  "group.id": process.env.GROUP_ID_ELASTIC,
+  "metadata.broker.list": process.env.CLOUDKARAFKA_BROKERS.split(","),
+  "socket.keepalive.enable": true,
+  "security.protocol": "SASL_SSL",
+  "sasl.mechanisms": "SCRAM-SHA-256",
+  "sasl.username": process.env.CLOUDKARAFKA_USERNAME,
+  "sasl.password": process.env.CLOUDKARAFKA_PASSWORD,
+  debug: "generic,broker,security",
+};
+const kafkaConfigRedis = {
+  "group.id": process.env.GROUP_ID_REDIS,
   "metadata.broker.list": process.env.CLOUDKARAFKA_BROKERS.split(","),
   "socket.keepalive.enable": true,
   "security.protocol": "SASL_SSL",
@@ -9,5 +19,4 @@ const kafkaConf = {
   debug: "generic,broker,security",
 };
 
-
-module.exports = kafkaConf;
+module.exports = { kafkaConfigElastic, kafkaConfigRedis };
--- src/server/ServingLayer/model/Kafka.js
+++ src/server/ServingLayer/model/Kafka.js
@@ -3,22 +3,39 @@ const kafkaConf = require("../config/kafka.config");
 
 const orderTopic = process.env.KAFKA_ORDER_TOPIC;
 const deliveredTopic = process.env.KAFKA_DELIVERED_TOPIC;
-const consumer = new Kafka.KafkaConsumer(kafkaConf);
+const elasticConsumer = new Kafka.KafkaConsumer(kafkaConf.kafkaConfigElastic);
+const redisConsumer = new Kafka.KafkaConsumer(kafkaConf.kafkaConfigRedis);
 
-consumer
+elasticConsumer
   .on("ready", function () {
     console.log("Elastic Consumer ready");
-    consumer.subscribe([orderTopic, deliveredTopic]);
-    consumer.consume();
+    elasticConsumer.subscribe([orderTopic, deliveredTopic]);
+    elasticConsumer.consume();
   })
   .on("disconnected", (arg) => {
-    console.log("Consumer ${arg} disconnected.");
-    consumer.connect();
+    console.log("Elastic consumer ${arg} disconnected.");
+    elasticConsumer.connect();
   })
   .on("event.error", (err) => {
-    console.log("Error from consumer:", err.message);
-    consumer.disconnect();
+    console.log("Error from elastic consumer:", err.message);
+    elasticConsumer.disconnect();
   })
   .connect();
 
-module.exports = consumer;
+redisConsumer
+  .on("ready", function () {
+    console.log("Redis consumer ready");
+    redisConsumer.subscribe([orderTopic, deliveredTopic]);
+    redisConsumer.consume();
+  })
+  .on("disconnected", (arg) => {
+    console.log("Redis consumer ${arg} disconnected.");
+    elasticConsumer.connect();
+  })
+  .on("event.error", (err) => {
+    console.log("Error from redis consumer:", err.message);
+    elasticConsumer.disconnect();
+  })
+  .connect();
+
+module.exports = { elasticConsumer, redisConsumer };
